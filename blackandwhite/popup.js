f5599895d55e4dac41146f737126e5d63c73d737
manon-coulombe <manon.coulombe@student.adatechschool.fr>
2020-10-22 14:30:36 +0200

    add button activate

--- blackandwhite/popup.js
+++ blackandwhite/popup.js
@@ -1,1 +1,6 @@
-document.addEventListener("click", (e) => {
+let buttonActivate = document.getElementById('activate');
+
+buttonActivate.addEventListener("click", function() {
+      console.log('activate');
+      browser.tabs.executeScript(null, { file: "blackandwhite.js" });
+  });
