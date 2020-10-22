f022414e796fedacb4854c2d23c7283a84ff8428
manon-coulombe <manon.coulombe@student.adatechschool.fr>
2020-10-22 16:03:40 +0200

    added clear

--- blackandwhite/popup.js
+++ blackandwhite/popup.js
@@ -1,6 +1,12 @@
 let buttonActivate = document.getElementById('activate');
+let buttonDesactivate = document.getElementById('desactivate');
 
 buttonActivate.addEventListener("click", function() {
       console.log('activate');
       browser.tabs.executeScript(null, { file: "blackandwhite.js" });
-  });
+});
+
+buttonDesactivate.addEventListener("click", function() {
+      console.log('desactivate');
+      browser.tabs.reload();
+});
